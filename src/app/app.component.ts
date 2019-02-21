f04905d691b580a118df0ae0dc2cdfdece87e834
Luis Enrique Mtz <luiseblake@live.com>
2019-02-21 17:10:03 -0600

    Primer subida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
-}
+
+  palabra = 'SANDIA';
+  palabraOculta = '';
+  intentos = 0;
+  gano = false;
+  perdio = false;
+  letrapres = true;
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor () {
+    this.palabraOculta = '* '.repeat ( this.palabra.length);
+  }
+
+  comprobar ( letra ) {
+
+    this.existeLetra(letra);
+
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+
+    for ( let i = 0; i < this.palabra.length; i++) {
+
+      if ( this.palabra[i] === letra ) {
+        palabraOcultaArr[i] = letra;
+
+      }
+    }
+      this.palabraOculta = palabraOcultaArr.join(' ');
+      this.verificaGane();
+
+  }
+
+  verificaGane() {
+      const palabraArr = this.palabraOculta.split (' ');
+      const palabraEvaluar = palabraArr.join('');
+
+      if (palabraEvaluar === this.palabra ) {
+        this.gano = true;
+        console.log('GANASTE MEN');
+      }
+
+      if (this.intentos >= 9) {
+        this.perdio = true;
+        console.log('PERDISTE MEN');
+
+      }
+  }
+
+  existeLetra ( letra ) {
+    if (this.palabra.indexOf(letra) >= 0 ) {
+     // console.log('Letra ' + letra, ' existe');
+    } else {
+     // console.error('Letra ' + letra, ' NO existe');
+      this.intentos++;
+    }
+
+
+  } }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+} from '@angular/material';
+
 
 @NgModule({
+
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
+
+
