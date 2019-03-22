a0e74e386cab62ccc953c22690726dae4aacad3d
Luis Enrique Mtz <luiseblake@live.com>
2019-03-22 17:17:40 -0600

    Campos Autorizados

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -4,31 +4,80 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
-  styles: []
+  styles: [`
+  .ng-invalid.ng-touched:not(form) {
+      border: 2px solid red;
+  }
+  `]
 })
 export class DataComponent {
 
-  forma:FormGroup;
+   formato = [{
+    codigo: 'rev',
+    nombre: 'Revista',
+  } , {
+    codigo: 'cap',
+    nombre: 'Capsula',
+  } , {
+    codigo: 'mus',
+    nombre: 'Musical'
+  } , {
+    codigo: 'hab',
+    nombre: 'Hablado'
+  } , {
+    codigo: 'otro',
+    nombre: 'Otro',
+  }];
+  
+
+  tipopar = [{
+    codigo: 'ciu',
+    nombre: 'Ciudadano',
+  } , {
+    codigo: 'est',
+    nombre: 'Estudiante',
+  } , {
+    codigo: 'AC',
+    nombre: 'Asociación Civil',
+  } , {
+    codigo: 'CC',
+    nombre: 'Colectivo Cultural',
+  } , {
+    codigo: 'OT',
+    nombre: 'Otro',
+    
+  }];
 
 
-  constructor() { 
+  forma: FormGroup;
+
+
+  constructor() {
   this.forma = new FormGroup( {
-    'nombre': new FormControl ('Luisillo'),
-    'apellidos': new FormControl (''),
-    'Correo Electronico': new FormControl (''),
-    'Telefono Directo': new FormControl (''),
-    'Direccion': new FormControl (''),
-    'Delegacion': new FormControl (''),
-    'C.P.': new FormControl (''),
+    'seudonimo': new FormControl ('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
+    'participantes': new FormControl ('', [Validators.required, Validators.min(1), Validators.max(5)] ),
+    'tema': new FormControl ('', Validators.required),
+    'tipopar': new FormControl ('{{ tipopar.nombre }}', Validators.required),
+    'programa': new FormControl ('', Validators.required),
+    'objetivo': new FormControl ('', Validators.required),
+    'formato': new FormControl ( '{{ formato.nombre }}', Validators.required),
+    'CorreoElectronico': new FormControl ('', [Validators.required, Validators.email]),
+    'caltem1': new FormControl ('', Validators.required),
+    'caltem2': new FormControl ('', Validators.required),
+    'caltem3': new FormControl ('', Validators.required),
+    'caltem4': new FormControl ('', Validators.required),
+    'caltem5': new FormControl ('', Validators.required),
+    'rectec': new FormControl ('', Validators.required),
+    'antecedentes': new FormControl ('', Validators.required),
 
 
-  })
+  });
 
 
 
   }
 
-      guardarCambios(){
+      guardarCambios() {
         console.log ( this.forma.value );
       }
 }
